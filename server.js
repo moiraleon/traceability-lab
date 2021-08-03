6c5e97f0fa6843e4f7c96e0971a465cf64a81342
Moira Leon <moira.leon2@gmail.com>
2021-08-03 15:52:28 -0600

    Primus

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require ('express');
 const app = express();
 const path = require('path');
-var Rollbar = require('rollbar')
+var Rollbar = require("rollbar");
 var rollbar = new Rollbar({
-  accessToken: '0144ed4b7fbb4cfb863b49dbacc2a459',
+  accessToken: 'a8f70a4ed99e49ad94738657595719df',
   captureUncaught: true,
-  captureUnhandledRejections: true,
-})
+  captureUnhandledRejections: true
+});
 
-// record a generic message and send it to Rollbar
-rollbar.log('yasss!')
+
 
 app.use(express.json());
 
@@ -30,13 +29,17 @@ const index =icecream.findIndex((icecream)=>{
 try {
     if (index === -1 && type !== "") {
       icecream.push(type);
+      rollbar.info('An icecream has been added')
       res.status(200).send(icecream);
     } else if (type === "") {
+        rollbar.error('Someone entered an invalid input')
       res.status(400).send("must provide an icecream type");
     } else {
+        rollbar.error('Someone has entered an already listed input')
       res.status(400).send("that icecream is already listed");
     }
   } catch (err) {
+      rollbar.error(err)
     console.log(err)
   }
 const port =process.env.PORT ||5040
