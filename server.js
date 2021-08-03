0a9eb9071140110b19533c8507c4fff480a32332
Moira Leon <moira.leon2@gmail.com>
2021-08-03 14:47:16 -0600

    primus

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
 const express = require ('express');
 const app = express();
+const path = require('path');
+
+app.use(express.json());
+
+const icecream =['chocolate', 'pistachio', 'mango sorbet']
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/api/students', function(req,res){
+    let {type} =req.body;
+    console.log(type)
+})
+
+
+
+
+
 const port =process.env.PORT ||5040
 
 app.listen(port, function(){
